ef5614318dac147fefc41745989bedb86befca70
João Artur <jartur.dev@gmail.com>
2022-05-11 17:28:50 -0300

    Lendo arquivo csv sem uso do scanner

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,8 @@
 package main.java;
 
-import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.nio.file.Path;
 import java.util.List;
-import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -14,27 +11,23 @@ public class App {
     var objectMapper = new ObjectMapper();
 
     try {
-      var json = Files.readString(Paths.get("automaton.json"));
+      var json = Files.readString(Path.of("automaton.json"));
 
       var config = objectMapper.readValue(json, AutomatonConfig.class);
 
       var automaton = new Automaton(config);
 
-      Scanner scanner = new Scanner(new File("transition.csv"));
+      var csv = Files.readString(Path.of("transition.csv"));
 
-      List<Transition> transitions = new ArrayList<>();
-
-      // Read line
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-
+      var transitions = List.of(csv.split("\n")).stream().map(line -> {
         var lineItems = line.split(",");
-        Transition transition = new Transition(lineItems[0], lineItems[1], lineItems[2],
-            lineItems[3], List.of(lineItems[4].split(" ")));
-
-        transitions.add(transition);
-
-      }
+        return new Transition(
+            lineItems[0],
+            lineItems[1],
+            lineItems[2],
+            lineItems[3],
+            List.of(lineItems[4].split(" ")));
+      }).toList();
 
       System.out.println(transitions);
 
