d9afc24d8f63da45b4743770a05463b2093d45d8
guilermmm <guilherme.r.a.melo@gmail.com>
2022-05-11 17:50:57 -0300

    verificação de atributos do automato 🤝

--- src/main/java/Automaton.java
+++ src/main/java/Automaton.java
@@ -11,12 +11,32 @@ public class Automaton {
   // private final Set<String> acceptingStates;
 
   public Automaton(AutomatonConfig config) throws IllegalAutomatonConfiguration {
+
+    for (var symbol : config.inputAlphabet()) {
+      if (symbol.length() != 1) {
+        throw new IllegalAutomatonConfiguration("Input Alphabet must be 1 character long each symbol", config);
+      }
+    }
+
+    for (var symbol : config.stackAlphabet()) {
+      if (symbol.length() != 1) {
+        throw new IllegalAutomatonConfiguration("Stack Alphabet must be 1 character long each symbol", config);
+      }
+    }
+
     if (!config.states().contains(config.initialState())) {
       throw new IllegalAutomatonConfiguration("Initial State is not present in List of States", config);
     }
 
     if (!config.stackAlphabet().contains(config.initialStackSymbol())) {
       throw new IllegalAutomatonConfiguration("Initial Stack Symbol is not present in Stack Alphabet", config);
+    }
+
+    for (var state : config.acceptingStates()) {
+      if (!config.states().contains(state)) {
+        throw new IllegalAutomatonConfiguration("Accepting State " + state + " is not present in List of States",
+            config);
+      }
     }
 
     // this.states = config.states();
